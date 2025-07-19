7a8ae85dfbacddc001a7d0252a78834704446b0e
heypandya <en.pandiyan@gmail.com>
2025-07-19 09:47:20 +0530

    Updated

--- Real-Time-Character-Counter/index.js
+++ Real-Time-Character-Counter/index.js
@@ -1,16 +1,12 @@
 const textAreaElement = document.getElementById("textarea");
 const totalCounterElement = document.getElementById("total-counter");
-const remainingCounterElement = document.getElementById('remaining-counter');
 
-textAreaElement.addEventListener('keyup', () => {
-  updateCounter()
+textAreaElement.addEventListener("input", () => {
+  updateCounter();
 });
 
 updateCounter();
- 
-function updateCounter() {
-  totalCounterElement.innerText = textAreaElement.value.length
-  remainingCounterElement.innerText = textAreaElement.getAttribute("maxLength") - textAreaElement.value.length;
-}
 
- 
+function updateCounter() {
+  totalCounterElement.innerText = textAreaElement.value.length;
+}
